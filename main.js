03937ef714b3ea5a63ac1e2619110e536e37542c
William Horn <williamjosephhorn@gmail.com>
2022-03-04 02:25:23 -0500

    added utility functions

--- main.js
+++ main.js
@@ -42,12 +42,98 @@ https://github.com/william-horn/my-coding-conventions/blob/main/document-convent
 ==================================================================================================================================
 */
 
-const outputDisplayContainer = document.querySelector(".output-display-container")
-const generateSearchButton = document.querySelector("#generate-search-btn")
+/* ------------ */
+/* Program Init */
+/* ------------ */
 
-function generateSearch() {
-    
+// get html elements refs
+const searchOutput = document.querySelector("#search-output");
+const wordInput = document.querySelector("#word-input");
+const stringLengthInput = document.querySelector("#length-input");
+const generateSearchButton = document.querySelector("#generate-search-btn");
+
+// store default css property states
+let savedElementPropertyDefaults = {};
+
+/* ----------------- */
+/* Utility Functions */
+/* ----------------- */
+
+function debounce(func, delay) {
+    let gate = true;
+    return function(...args) {
+        if (gate === true) {
+            gate = false;
+            func(...args);
+            setTimeout(() => {
+                gate = true
+            }, delay)
+        }
+    }
 }
 
-generateSearchButton.addEventListener("click", generateSearch)
+// return a random integer ranging from min to max
+function randomInt(min, max) {
+    if (!max) {
+        max = min;
+        min = 0;
+    }
+
+    const rand = Math.random();
+    return Math.floor(min*(1 - rand) + rand*max);
+}
+
+/* ------------------- */
+/* Dedicated Functions */
+/* ------------------- */
+
+function invalidSearchMessage(message) {
+    const oldText = generateSearchButton.textContent;
+    let oldBackgroundColor = document.querySelector(".generate-search-btn")
+
+    console.log($(".generate-search-btn").css("background-color"));
+    generateSearchButton.textContent = message;
+    // generateSearchButton.style.backgroundColor = "var(--theme-error-background-color)";
+
+    setTimeout(function() {
+        $(".generate-search-btn").removeClass("generate-search-btn-invalid")
+        // generateSearchButton.style.backgroundColor = oldBackgroundColor
+        generateSearchButton.textContent = oldText
+    }, 2000)
+}
+
+// generate random lower-case letters from ASCII code
+function getRandomLetterLower() {
+    return String.fromCharCode(randomInt(97, 122));
+}
+
+function generateSearch() {
+    const wordInputValue = wordInput.value;
+    const stringLengthValue = parseInt(stringLengthInput.value);
+
+    // handle no word input or no length input
+    if (!wordInputValue || isNaN(stringLengthValue)) {
+        invalidSearchMessage("Invalid data. Try again.");
+        return;
+    } else if (stringLengthValue > 200) {
+        invalidSearchMessage("String length is too large.");
+        return;
+    }
+}
+
+// load default css properties into 'savedElementPropertyDefaults'
+function loadElementPropertyDefaults() {
+    savedElementPropertyDefaults[generateSearchButton] = getComputedStyle(generateSearchButton);
+}
+
+// invoke css default property loader once the page loads
+document.addEventListener("readystatechange", event => {
+    if (document.readyState === "complete") {
+        loadElementPropertyDefaults();
+    }
+})
+
+// generate search click listener //
+const generateSearchDebounce = debounce(generateSearch, 2000);
+generateSearchButton.addEventListener("click", generateSearchDebounce);
 
